eb18ac7c5d303ba62c63f7495f9f324222d0738d
rbMangolin <rbmangolin@gmail.com>
2015-05-25 22:56:09 -0300

    Modificado a estrutura de lista, e criada a lista de id de grupode raça e de individuo por raça, e a busca também

--- TrabORD/trabalho.c
+++ TrabORD/trabalho.c
@@ -2,34 +2,64 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct lista_num{
-    int chave;
+struct lista{
+    char chave[100];
     int offset;
+    int prox_f;
+    int prox_i1;
+    int prox_i2;
 };
 
-//########
+
 int main(){
     int escolha;
-    struct lista_num indice_ID_R[100];
-    int count_ID_R = 0;
-    struct lista_num indice_ID_I[100];
-    int count_ID_I = 0;
+
+    //lista de id de raças
+    struct lista lIdR[100];
+    int cIdR = 0;
+
+    struct lista lIdG[100];
+    int cIdG = 0;
+    /*lista invertida de raças por id de grupo de raças
+
+    int cAuxIdG = 0;
+    struct lista_invertida_num lAuxIdG[100];*/
+
+    //lista de id de individuos
+    struct lista lIdI[100];
+    int cIdI = 0;
+
+    /*lista invertida de Individuos por id de raças
+    struct lista_invertida_num lIdIR[100];
+    int cIdIR = 0;
+    int cAuxIdIR = 0;
+    struct lista_invertida_num lAuxIdIR[100];*/
 
     do{
     	escolha = escolheOpcao();
     	switch(escolha){
     		case 1:
-                converterArquivo("individuos.txt", indice_ID_I, &count_ID_I);
+                converterArquivo("individuos.txt", lIdI, &cIdI,lIdR, &cIdR/*, lIdIR, lAuxIdIR, &cIdIR, &cAuxIdIR,NULL,NULL,NULL,NULL*/);
                 break;
             case 2:
-                converterArquivo("racas-grupos.txt", indice_ID_R, &count_ID_R);
+                converterArquivo("racas-grupos.txt", lIdR, &cIdR, lIdG, &cIdG);
                 break;
             case 3:
-                buscarIndicePorNumero(indice_ID_I,count_ID_I,"Digite o id do individuo que deseja recuperar:","individuos.txt");
+                buscarPorIndice(lIdI, cIdI,"Digite o id do individuo que deseja recuperar:","individuos.txt",NULL);
+                break;
+            case 4:
+                buscarPorIndice(lIdR, cIdR,"Digite o id da raça que deseja recuperar:","racas-grupos.txt",NULL);
+                break;
+            case 5:
+                buscarPorListaInvertida(lIdG, lIdR, cIdG,"Digite o id do grupo de raças das raças que deseja recuperar:","racas-grupos.txt",NULL);
+                break;
+            case 6:
+                buscarPorListaInvertida(lIdR, lIdI, cIdR,"Digite o id da raças dos individuos deseja recuperar:","individuos.txt",NULL);
                 break;
     	}
     }while(escolha != 0);
 }
+
 void mostrarOpcoes(){
 	printf("              /\\___/\\ \n");
 	printf("              `)o o('  \n");
@@ -40,9 +70,13 @@ void mostrarOpcoes(){
 	printf("1- Converter Arquivo individuo.txt\n");
     printf("2- Converter Arquivo racas-grupos.txt\n");
     printf("3- Buscar individuo por id\n");
+    printf("4- Buscar raca por id\n");
+    printf("5- Buscar racas por id do grupo de raca\n");
+    printf("6- Buscar individuos por id de raca\n");
 	// outras operacoes
 	printf("0- Sair\n");
 }
+
 int escolheOpcao(){
 	int operacao;
 
@@ -50,21 +84,21 @@ int escolheOpcao(){
 		mostrarOpcoes();
 		scanf("%i", &operacao);
         getchar();
-		if(operacao > 3 || operacao < 0){
+		if(operacao > 6 || operacao < 0){
 			printf("Opcao invalida\n");
 		}
-	}while(operacao > 3 || operacao < 0);
+	}while(operacao > 6 || operacao < 0);
 	return operacao;
 }
 
-void converterArquivo(char nome[], struct lista_num list_ID_I[], int* qntd){
+void converterArquivo(char nome[], struct lista list_id[], int *count_id, struct lista list_i1[], int *count_i1/*, struct lista_invertida_num list_i1_aux[], int *count_i1, int *count_i1_aux, struct lista_invertida_num list_i2[], struct lista_invertida_num list_i2_aux[], int *count_i2, int *count_i2_aux*/){
 	FILE *fimport;
     FILE *fimportaux;
 	FILE *faux;
 
     char aux, field_buffer[400], buffer[400];
     int count = 0, i = 0, offset_count = 0;
-    *qntd = 0;
+
     short length;
 
 	if ((fimport = fopen(nome,"r+")) == NULL) {
@@ -91,16 +125,25 @@ void converterArquivo(char nome[], struct lista_num list_ID_I[], int* qntd){
             count++;
             aux = fgetc(fimport);
             if(count == 1){
-                list_ID_I[*qntd].chave = atoi(field_buffer);
-                list_ID_I[*qntd].offset = offset_count;
-                *qntd+=1;
+                char string[100];
+                if(strlen(field_buffer)==1){
+                    string[0] = '0';
+                    string[1] = '\0';
+                } else{
+                    string[0]='\0';
+                }
+                strcat(string,field_buffer);
+                strcpy(list_id[*count_id].chave,string);
+                list_id[*count_id].offset = offset_count;
             }else if(count == 4){
             	length = strlen(buffer);
+            	//puts(buffer);
             	fwrite(&length, 1, sizeof(length), faux);
             	fwrite(buffer, 1, length, faux);
             	offset_count+=length+2;
             	buffer[0]='\0';
             	count = 0;
+            	*count_id += 1;
             }
     	}else{
         	i = 0;
@@ -121,23 +164,34 @@ void converterArquivo(char nome[], struct lista_num list_ID_I[], int* qntd){
     }
     remove("aux.txt");
 
-    quickSort(list_ID_I,0,*qntd-1);
+
+    quickSort(list_id,0,*count_id-1);
     fclose(fimportaux);
+
+    if(strcmp(nome,"individuos.txt")== 0){
+    //gerar lista de individuos por
+        gerarListaInvertida(list_i1, count_i1, list_id, count_id, "individuos.txt", 2);
+    }else{
+    //gerar lista de raças por grupo de raças
+        gerarListaInvertida(list_i1, count_i1, list_id, count_id, "racas-grupos.txt", 3);
+    }
+
 }
 
-void trocaValores(struct lista_num* a, struct lista_num* b){
-    struct lista_num aux;
+void trocaValores(struct lista* a, struct lista* b){
+    struct lista aux;
     aux = *a;
     *a = *b;
     *b = aux;
 }
 
-int divide(struct lista_num vec[], int esquerdo, int direito){
+int divide(struct lista vec[], int esquerdo, int direito){
     int i, j;
 
     i = esquerdo;
     for (j = esquerdo + 1; j <= direito; ++j){
-        if (vec[j].chave < vec[esquerdo].chave){
+        int aux = strcmp(vec[j].chave, vec[esquerdo].chave);
+        if (aux < 0){
             ++i;
             trocaValores(&vec[i], &vec[j]);
         }
@@ -147,7 +201,7 @@ int divide(struct lista_num vec[], int esquerdo, int direito){
     return i;
 }
 
-void quickSort(struct lista_num vec[], int esquerdo, int direito){
+void quickSort(struct lista vec[], int esquerdo, int direito){
     int r;
     if (direito > esquerdo){
         r = divide(vec, esquerdo, direito);
@@ -156,13 +210,25 @@ void quickSort(struct lista_num vec[], int esquerdo, int direito){
     }
 }
 
-void buscarIndicePorNumero(struct lista_num list[], int count, char frase[], char arqName[]){
-    int id;
+void buscarPorIndice(struct lista list[], int count, char frase[], char arqName[], char indice[100]){
+    char valor[100];
+    if(frase == NULL){
+        strcpy(valor,indice);
+    }else{
+        puts(frase);
+        gets(valor);
+        char string[100];
+        if(strlen(valor)==1){
+            string[0] = '0';
+            string[1] = '\0';
+        } else{
+            string[0]='\0';
+        }
+        strcat(string,valor);
+        strcpy(valor,string);
+    }
 
-    puts(frase);
-    scanf("%i", &id);
-    int aux = buscaBinariaNumero(list,0,count-1,id);
-
+    int aux = buscaBinariaNumero(list,0,count-1,valor);
 
     if(aux != -1){
         FILE *fp;
@@ -173,22 +239,76 @@ void buscarIndicePorNumero(struct lista_num list[], int count, char frase[], cha
         if(fseek(fp,list[aux].offset, SEEK_SET) == 0){
             short length;
             char buffer[500];
+            buffer[0]='\0';
             printf("offset -- %i\n", list[aux].offset);
             fread(&length, 1, sizeof(length),fp);
             fread(buffer, 1, length, fp);
-
+            buffer[strlen(buffer)]='\0';
             printf("registro %s, length %i\n", buffer, length);
         }
+    }else {
+        printf("     ____  ____  ____ \n");
+        printf("    ||4 ||||0 ||||4 ||\n");
+        printf("    ||__||||__||||__||\n");
+        printf("    |/__\\||/__\\||/__\\|\n\n    File not found\n");
+
     }
 
 }
 
-int buscaBinariaNumero(struct lista_num list[], int inicio, int fim, int value){
+void buscarPorListaInvertida(struct lista list[], struct lista list2[], int count, char frase[], char arqName[], char indice[100]){
+    char valor[100];
+    if(frase == NULL){
+        strcpy(valor,indice);
+    }else{
+        puts(frase);
+        gets(valor);
+        char string[100];
+        if(strlen(valor)==1){
+            string[0] = '0';
+            string[1] = '\0';
+        } else{
+            string[0]='\0';
+        }
+        strcat(string,valor);
+        strcpy(valor,string);
+    }
+
+    int aux = buscaBinariaNumero(list,0,count-1,valor);
+
+    if(aux != -1){
+        FILE *fp;
+        if ((fp = fopen(arqName,"r+")) == NULL) {
+            printf("Meça as tentativa de abrir seus arquivos Parça!");
+            exit(1);
+        }
+        int j = list[aux].prox_f;
+        while(j!=-1){
+            short length;
+            char buf[500];
+            fseek(fp,list2[j].offset, SEEK_SET);
+            fread(&length, 1, sizeof(length),fp);
+            fread(buf, 1, length, fp);
+            buf[length] = '\0';
+            printf("%s\n", buf);
+            j=list2[j].prox_i1;
+        }
+    }else {
+        printf("     ____  ____  ____ \n");
+        printf("    ||4 ||||0 ||||4 ||\n");
+        printf("    ||__||||__||||__||\n");
+        printf("    |/__\\||/__\\||/__\\|\n\n    File not found\n");
+
+    }
+}
+
+int buscaBinariaNumero(struct lista list[], int inicio, int fim, char value[]){
     if(inicio <= fim){
         int aux =((fim-inicio)/2)+inicio;
-        if(list[aux].chave > value){
+        int cmp = strcmp(list[aux].chave,value);
+        if(cmp > 0){
             return buscaBinariaNumero(list,inicio,aux-1,value);
-        }else if(list[aux].chave < value){
+        }else if(cmp < 0){
             return buscaBinariaNumero(list,aux+1,fim,value);
         }else{
             return aux;
@@ -196,5 +316,87 @@ int buscaBinariaNumero(struct lista_num list[], int inicio, int fim, int value){
     }else{
         return -1;
     }
+}
+
+int caminharListaInvertida(struct lista list[], int indice){
+    printf("%s -->", list[indice].chave);
+    if(list[indice].prox_i1 == -1){
+        printf("\n\n");
+        return indice;
+    }else{
+        return caminharListaInvertida(list, list[indice].prox_i1);
+    }
+}
+
+void gerarListaInvertida(struct lista lista1[], int *count1, struct lista lista2[], int *count2, char nome[], int breakPoint){
+    FILE *fp;
+    if ((fp = fopen(nome,"r+")) == NULL) {
+        printf("Meça as tentativa de abrir seus arquivos Parça!");
+        exit(1);
+    }
+
+    int i;
+
+    for(i=0;i<*count2;i++){
+        fseek(fp,lista2[i].offset, SEEK_SET);
+        short length;
+        char buf[500];
+        fread(&length, 1, sizeof(length),fp);
+        fread(buf, 1, length, fp);
+        char *token;
+        int count = 0;
+        buf[length] = '\0';
+        printf("%s\n",buf);
+        token = strtok(buf, "|");
+
+
+        while( token != NULL ){
+            token[strlen(token)] = '\0';
+            count++;
+            if(count== breakPoint){
+
+                char string[100];
+                if(strlen(token)==1){
+                    string[0] = '0';
+                    string[1] = '\0';
+                } else{
+                    string[0]='\0';
+                }
+                strcat(string,token);
+                int aux = buscaBinariaNumero(lista1,0,*count1,string);
+                printf("%s    %i\n\n", string, aux);
+                if(aux==-1){
+                    strcpy(lista1[*count1].chave,string);
+                    lista1[*count1].prox_f = i;
+                    quickSort(lista1,0,*count1);
+                    *count1+=1;
+                }else{
+                    aux = caminharListaInvertida(lista2,lista1[aux].prox_f);
+                    lista2[aux].prox_i1 = i;
+                }
+
+                lista2[i].prox_i1 = -1;
+            }
+            token = strtok(NULL, "|");
+        }
+    }
+
+    /*for(i=0;i<*count1;i++){
+        int j=lista1[i].prox_f;
+        printf("%s\n",lista1[i].chave);
+        while(j!=-1){
+            short length;
+            char buf[500];
+            fseek(fp,lista2[j].offset, SEEK_SET);
+            fread(&length, 1, sizeof(length),fp);
+            fread(buf, 1, length, fp);
+            //buf[length] = '\0';
+            printf("%s -->", buf);
+            j=lista2[j].prox_i1;
+        }
+        puts("\n\n");
+    }*/
+
+    fclose(fp);
 }
 
